7a4e961192edd10f55feeafaf38f3e287c490e38
Max Achenbach <max@maxachenbach.de>
2023-07-01 23:55:26 +0200

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ichwillkeinenaccount.github.io'
+  site: 'https://ichwillkeinenaccount.github.io',
+  base: '/hoehenrettung'
 });
