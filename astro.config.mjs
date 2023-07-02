396c69cc9d2f9aca047c97534226830a0c079e15
Max Achenbach <max@maxachenbach.de>
2023-07-03 00:11:54 +0200

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'teest.hoehenrettung-bs.de'
+  site: 'https://teest.hoehenrettung-bs.de'
 });
