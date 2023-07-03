bf8c08e39a5ad63fb8c1620ab0addfb6ee9e3772
Max Achenbach <max@maxachenbach.de>
2023-07-03 16:34:28 +0200

    use correct preview domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://teest.hoehenrettung-bs.de'
+  site: 'https://preview.hoehenrettung-bs.de/'
 });
