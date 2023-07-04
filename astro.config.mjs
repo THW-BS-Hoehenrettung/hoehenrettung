f069f5b523153a0665dcb6524edce07d4fe822f4
ichwillkeinenaccount <56072922+ichwillkeinenaccount@users.noreply.github.com>
2023-07-04 22:11:53 +0200

    fix dev container error; workaround for image problem after removing sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,9 +22,7 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+    image(),
     AutoImport({
       imports: [
         "@/shortcodes/Button",
