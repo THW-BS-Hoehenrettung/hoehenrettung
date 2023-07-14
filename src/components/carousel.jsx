bcfdf5f658ccde92eb506f8b91765653e581e2f8
ChsRmb <chsrmb@chsrmb.de>
2023-07-15 01:44:41 +0200

    chore: prettier

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,48 +1,35 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
 import pictures from "@/config/carousel.json";
 
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-// import './styles.css';
-
-// import required modules
-import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules';
-
-
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules";
 
 export default function BannerCarousel() {
   return (
-      <Swiper
-        spaceBetween={30}
-        loop={true}
-        effect={'fade'}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, EffectFade, Pagination, Navigation]}
-        className="rounded-2xl"
-      >
-        {
-        pictures.images.map((img) => (
-          <SwiperSlide>
-            <img src={img} alt={img} />
-          </SwiperSlide>
-        ))
-        }
-      </Swiper>
+    <Swiper
+      spaceBetween={30}
+      loop={true}
+      effect={"fade"}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, EffectFade, Pagination, Navigation]}
+      className="rounded-2xl"
+    >
+      {pictures.images.map((img) => (
+        <SwiperSlide>
+          <img src={img} alt={img} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
