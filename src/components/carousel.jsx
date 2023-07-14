c68ea8cfb17d4b766599ac97196746a808a96ba2
ChsRmb <chsrmb@chsrmb.de>
2023-07-15 01:22:59 +0200

    feat: add functioning banner carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -7,39 +7,42 @@ import pictures from "@/config/carousel.json";
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import './styles.css';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules';
 
-export default function Carousel() {
-  // @ts-ignore
+
+
+export default function BannerCarousel() {
   return (
-    <>
       <Swiper
         spaceBetween={30}
+        loop={true}
         effect={'fade'}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
+        modules={[Autoplay, EffectFade, Pagination, Navigation]}
+        className="rounded-2xl"
       >
+        {
         pictures.images.map((img) => (
           <SwiperSlide>
             <img src={img} alt={img} />
           </SwiperSlide>
         ))
+        }
       </Swiper>
-    </>
   );
 }
